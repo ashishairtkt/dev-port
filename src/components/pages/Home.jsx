cf00f3ff4ae159037e040b39f0d0c465cc3e8a03
ashish <ashish@airtkt.com>
2024-02-28 12:45:07 +0530

    add animation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,16 +9,19 @@ export default function Home() {
           <Col xs={12} md={6}>
             <div className="headline">
               <h1>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Laboriosam, .
+                Hi there! 👋
+                <br /> I'm Ashish Singh Rathod.
               </h1>
 
               <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Dolores possimus sint porro quis in excepturi laudantium
-                necessitatibus, deleniti voluptas sit odit delectus incidunt
-                inventore nihil, deserunt modi aperiam temporibus repellat!
+                A passionate frontend developer with a keen eye for creating
+                visually stunning and user-friendly web experiences. My journey
+                in the world of coding began with a fascination for turning
+                lines of code into beautiful, interactive websites that leave a
+                lasting impression.
               </p>
+
+              <p>Let's build something amazing together! 💻✨</p>
               <div>
                 <Button className="btnclick">Click here</Button>
               </div>
